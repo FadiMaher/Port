25c1aa9f0892cf5c3fb36093032ec284d1aea638
Fadi Maher <fadi-h-@hotmail.com>
2022-06-10 19:07:08 +0300

    Port added

---
+++ testing.js
@@ -0,0 +1,11 @@
+
+
+const scro=document.getElementById("scroll");
+const imag=document.getElementsByClassName("pimage");
+
+window.addEventListener('scroll', () => {
+console.log('sss');
+scro.style.display="block";
+
+})
+
